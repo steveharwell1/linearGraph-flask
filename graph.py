9e8f00ec05b62ea34ed9a27c0a6457e50a4bb916
Stephen Harwell <steveharwell@gmail.com>
2017-11-14 08:32:10 -0600

    Moved graph generation to get_image also added io.BytesIO for temp file so that we don't have to save graphs before serving them.

--- graph.py
+++ graph.py
@@ -4,6 +4,7 @@ from flask import request
 from flask import render_template
 app = Flask(__name__)
 
+import io
 import numpy as np
 import matplotlib.pyplot as plt
 from fractions import Fraction
@@ -13,20 +14,21 @@ from fractions import Fraction
 def hello():
     m = float(Fraction(request.args.get("m", default=1)))
     b = float(Fraction(request.args.get("b", default=0)))
-    path = graph(m, b)
+    path = "api/v1/graph/{}/{}".format(m, b)
     return render_template("index.html", image=path)
 
-@app.route('/get_image')
-def get_image():
-    filename = request.args.get('image')
-    return send_file(filename, mimetype='image/png')
+@app.route('/api/v1/graph/<float:m>/<float:b>')
+def get_image(m, b):
+    f = io.BytesIO()
+    graph(m, b, f)
+    f.seek(0)
+    return send_file(f, mimetype='image/png')
 
-
-def graph(m, b):
+def graph(m, b, fp):
     plt.clf()
     x = np.linspace(-10, 10, num=500)
     plt.plot(x, m*x + b)
     plt.ylabel("{}x+{}".format(m, b))
-    st = "img/{}{}.png".format(m, b)
-    plt.savefig(st)
-    return st
+    
+    plt.savefig(fp)
+    return fp
