1bb3bcb12baaeafb6888866afc65342da0047930
Stephen Harwell <steveharwell@gmail.com>
2019-09-26 10:49:52 -0500

    commiting what is in tinyurl

--- graphs.py
+++ graphs.py
@@ -1,8 +1,7 @@
 import os
 import sqlite3
 import requests
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-    render_template, flash, send_file
+from flask import Flask, request, redirect, url_for, render_template, send_file
 app = Flask(__name__)
 app.config.from_object(__name__)
 
@@ -31,29 +30,26 @@ def connect_db():
 @app.route("/", methods=["GET"])
 def index():
     try:
-        m = float(Fraction(request.args.get("m", default=1)))
+        m = Fraction(request.args.get("m", default=1))
     except(ValueError):
         m = 1.0
     try:
-        b = float(Fraction(request.args.get("b", default=0)))
+        b = Fraction(request.args.get("b", default=0))
     except(ValueError):
         b = 0.0
 
-    paths = list()
-    paths.append({"path": "api/v1/linear/graph/{}/{}".format(m, b),
-                    "m": "{:.2}".format(m), "b": b})
-    paths.append({"path": "api/v1/linear/graph/{}/{}".format(-m, b),
-                    "m": "{:.2}".format(-m), "b": b})
-    paths.append({"path": "api/v1/linear/graph/{}/{}".format(1 / m, b),
-                    "m": "{:.2}".format(1 / m), "b": b})
-    paths.append({"path": "api/v1/linear/graph/{}/{}".format(-1 / m, b),
-                    "m": "{:.2}".format(-1 / m), "b": b})
+    paths = [
+       {"path":url_for("get_image", m=esc(m), b=b), "m":m, "b":b},
+       {"path":url_for("get_image", m=esc(-m), b=b), "m":-m, "b":b},
+       {"path":url_for("get_image", m=esc(1/m), b=b), "m":1/m, "b":b},
+       {"path":url_for("get_image", m=esc(-1/m), b=b), "m":-1/m, "b":b},
+    ]
 
     qrcodes = [
-        "api/v1/linear/qrcode/{}/{}".format(m, b),
-        "api/v1/linear/qrcode/{}/{}".format(-m, b),
-        "api/v1/linear/qrcode/{}/{}".format(1 / m, b),
-        "api/v1/linear/qrcode/{}/{}".format(-1 / m, b)
+        url_for("get_qrcode", m=esc(m), b=b),
+        url_for("get_qrcode", m=esc(-m), b=b),
+        url_for("get_qrcode", m=esc(1/m), b=b),
+        url_for("get_qrcode", m=esc(-1/m), b=b),
     ]
 
     return render_template("index.html", paths=paths, qrcodes=qrcodes)
@@ -62,11 +58,11 @@ def index():
 @app.route('/api/v1/linear/graph/<m>/<b>')
 def get_image(m, b):
     try:
-        m = float(m)
+        m = float(Fraction(unescape(m)))
     except(ValueError):
         m = 1
     try:
-        b = float(b)
+        b = float(Fraction(unescape(b)))
     except(ValueError):
         b = 0.0
     f = io.BytesIO()
@@ -77,18 +73,9 @@ def get_image(m, b):
 
 @app.route('/api/v1/linear/qrcode/<m>/<b>')
 def get_qrcode(m, b):
-    try:
-        m = float(m)
-    except(ValueError):
-        m = 1
-    try:
-        b = float(b)
-    except(ValueError):
-        b = 0.0
-
     f = io.BytesIO()
     sitename = "http://192.168.0.13:5000"
-    path = "{}/api/v1/linear/graph/{}/{}".format(sitename, m, b)
+    path = url_for("get_image", m=m, b=b)
     make_qrcode(path, f)
     f.seek(0)
     return send_file(f, mimetype='image/png')
@@ -102,3 +89,9 @@ def make_qrcode(path, fp):
 def make_tinyurl(path):
     response = requests.get("http://www.tinyurl.com/api-create.php", params={"url": path})
     return response.content
+
+def esc(exp):
+    return str(exp).replace("/", "%2F")
+
+def unescape(exp):
+    return str(exp).replace("%2F" ,"/")
